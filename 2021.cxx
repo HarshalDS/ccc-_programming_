b7d3ba9e87e19f46570e0677c14113de39f1f9b8
HarshalDS <91478407+HarshalDS@users.noreply.github.com>
2021-09-29 08:32:08 +0530

    Add files via upload

---
+++ 2021.cxx
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a,b;
+	int x;
+	printf("Enter the first number:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+ x=a+b;
+ printf("The result is %d",x);
+ getch();
+}
+	
